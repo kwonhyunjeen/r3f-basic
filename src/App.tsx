077dd131e63ced49054e7003b4e7c7011120bb90
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2025-05-14 19:51:22 +0900

    Feat: add a basic example using the R3F core concepts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import "./App.css";
+import { Canvas } from '@react-three/fiber';
+import './modern-normalize.css';
+import Cube from './Cube';
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
-  return <></>;
+  return (
+    <Canvas
+      camera={{
+        fov: 75,
+        near: 1,
+        far: 100,
+        position: [5, 5, 5]
+      }}
+    >
+      <color attach="background" args={['white']} />
+      <OrbitControls />
+      <axesHelper args={[5]} />
+      <gridHelper args={[10, 10]} />
+      <Cube />
+    </Canvas>
+  );
 }
 
 export default App;
---
+++ src/Cube.tsx
@@ -0,0 +1,64 @@
+import * as THREE from 'three';
+import { useRef } from 'react';
+
+export default function Cube() {
+  const boxRef = useRef<THREE.Mesh>(null);
+  const groupRef = useRef<THREE.Group>(null);
+
+  return (
+    <>
+      <directionalLight position={[5, 5, 5]} />
+      <group
+        ref={groupRef}
+        position={[0, 0, 0]}
+        rotation={[
+          THREE.MathUtils.degToRad(0),
+          THREE.MathUtils.degToRad(45),
+          THREE.MathUtils.degToRad(0)
+        ]}
+      >
+        <axesHelper args={[5]} />
+        <mesh
+          ref={boxRef}
+          position={[0, 0, 0]}
+          scale={[1, 1, 1]}
+          rotation={[
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0)
+          ]}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
+        <mesh
+          ref={boxRef}
+          position={[0, 2, 0]}
+          scale={[1, 1, 1]}
+          rotation={[
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0)
+          ]}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="pink" />
+        </mesh>
+        <mesh
+          ref={boxRef}
+          position={[2, 0, 0]}
+          scale={[1, 1, 1]}
+          rotation={[
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(45),
+            THREE.MathUtils.degToRad(45)
+          ]}
+        >
+          <axesHelper args={[3]} />
+          <boxGeometry />
+          <meshStandardMaterial color="olive" />
+        </mesh>
+      </group>
+    </>
+  );
+}
