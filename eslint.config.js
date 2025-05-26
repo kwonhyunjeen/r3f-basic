8153bc10e3a80267c3efefc2f3f5ef512dbf6bce
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2025-05-26 14:22:52 +0900

    chore: set up prettier and eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,55 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import eslint from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import unicorn from "eslint-plugin-unicorn";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist", "dist-ssr"] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  unicorn.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
+      ],
+      "unicorn/filename-case": [
+        "error",
+        {
+          cases: {
+            camelCase: true,
+            kebabCase: true,
+            pascalCase: true,
+            snakeCase: false,
+          },
+        },
+      ],
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          replacements: {
+            env: false,
+            envs: false,
+            prop: false,
+            props: false,
+            ref: false,
+            refs: false,
+          },
+        },
       ],
     },
   },
-)
+);
