5bcd93fe4c8009d401a0185e168f5175c7366ca5
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2025-06-09 20:40:50 +0900

    chore: set up tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import checker from "vite-plugin-checker";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,5 +15,6 @@ export default defineConfig({
       },
     }),
     react(),
+    tailwindcss(),
   ],
 });
